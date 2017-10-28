7e412de3f5aaf56093f1578b8797a006a95e5234
Tylo Ren <tyreek.griffith>
2017-10-28 00:33:14 -0400

    Updated Models

--- rapidprototyping-django/myproject/todos/models.py
+++ rapidprototyping-django/myproject/todos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Todo(models.Model):
+class profile(models.Model):
     user =  models.ForeignKey(User, null=True, blank=True)
     description = models.CharField(max_length=128, null=False, blank=False)
     due_date = models.DateField(null=True, blank=True)
