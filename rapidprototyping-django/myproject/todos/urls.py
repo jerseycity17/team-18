d759081f6166f8c044f1f3012e7f61e107cfad66
Darshan Patel <darshan97@gmail.com>
2017-10-28 01:44:50 -0400

    reverting changes

--- rapidprototyping-django/myproject/todos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^todos/$', views.todo_list.as_view(), name='todo_list'),
-    url(r'^todo/create/$', views.todo_create.as_view(), name='todo_create')
-]
